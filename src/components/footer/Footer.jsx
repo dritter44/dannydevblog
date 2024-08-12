e94245d9571846f4bce94ad90d71e6da5c1763c9
dreither44 <daniel.reither@usmc.mil>
2024-08-11 23:21:09 -0500

    1:33:41 creating footer CSS.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+ import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 function Footer() {
     return (
-        <div className={styles.container}>Footer</div>
+        <div className={styles.container}>
+            <div className={styles.info}>
+                <div className={styles.logo}>
+                    <Image src="/logo.png" alt="Danny Dev blog" width={50} height={50} />
+                    <h1 className={styles.logoText}>Danny Dev Blog</h1>
+                </div>
+                <p className={styles.desc}>This is an info paragraph. This is an info paragraph.
+                This is an info paragraph.This is an info paragraph.This is an info paragraph.
+                This is an info paragraph.This is an info paragraph.This is an info paragraph.
+                </p>
+                <div className={styles.icons}>
+                    <Image src="/facebook.png" alt="" width={18} height={18} />
+                    <Image src="/instagram.png" alt="" width={18} height={18} />
+                    <Image src="/tiktok.png" alt="" width={18} height={18} />
+                    <Image src="/youtube.png" alt="" width={18} height={18} />
+                </div>
+            </div>
+            <div className={styles.links}>
+                <div className={styles.lists}>
+                    <span className={styles.listTitle}>Links</span>
+                    <Link href="/" className={styles.listItem} >Homepage</Link>
+                    <Link href="/" className={styles.listItem} >Blog</Link>
+                    <Link href="/" className={styles.listItem} >About</Link>
+                    <Link href="/" className={styles.listItem} >Contact</Link>
+                </div>
+                <div className={styles.lists}>
+                    <span className={styles.listTitle}>Tags</span>
+                    <Link href="/" className={styles.listItem} >Style</Link>
+                    <Link href="/" className={styles.listItem} >Fashion</Link>
+                    <Link href="/" className={styles.listItem} >Coding</Link>
+                    <Link href="/" className={styles.listItem} >Travel</Link>
+                </div>
+                <div className={styles.lists}>
+                    <span className={styles.listTitle}>Social</span>
+                    <Link href="/" className={styles.listItem} >Facebook</Link>
+                    <Link href="/" className={styles.listItem} >Instagram</Link>
+                    <Link href="/" className={styles.listItem} >Tiktok</Link>
+                    <Link href="/" className={styles.listItem} >YouTube</Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -2,27 +2,21 @@ import React from "react";
 import styles from "./menu.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import MenuPost from "../menuPosts/MenuPost";
+import MenuCategories from "../menuCategories/MenuCategories";
 
 function Menu() {
     return (
         <div className={styles.container}>
             <h2 className={styles.subtitle}>{"What's hot"}</h2>
             <h1 className={styles.title}>Most Popular</h1>
-            <div className={styles.items}>
-                <Link className={styles.item}>
-                    <div className={styles.imageContainer}>
-                        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-                    </div>
-                    <span className={`${styles.category} ${styles.travel}`}>
-                        Travel
-                    </span>
-                    <h3>Lorem ipsum this is a sentence!</h3>
-                    <div className={styles.detail}>
-                        <span className={styles.username}>John Doe</span>
-                        <span className={styles.date}>8/10/24</span>
-                    </div>
-                </Link>
-            </div>
+            <MenuPost withImage={false} />
+            <h2 className={styles.subtitle}>Discover by topic</h2>
+            <h1 className={styles.title}>Categories</h1>
+            <MenuCategories />
+            <h2 className={styles.subtitle}>Chosen by the Editor</h2>
+            <h1 className={styles.title}>Editors Top Picks</h1>
+            <MenuPost withImage={true} />
         </div>
     );
 };
